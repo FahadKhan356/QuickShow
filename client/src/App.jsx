47bd9ec81898c900556c8726b23f6159d9883aa0
Fahadkhan <fahadk8080@gmail.com>
2025-06-24 12:55:09 +0500

    worked on navbar and added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,47 @@
-import React from'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
-// import Home from './pages/Home'
-import Movies from './pages/Movies'
-import MoviesDetails from './pages/MovieDetails'
-import MyBookings from './pages/MyBookings'
-import SeatLayout from './pages/SeatLayout'
-import Favorite from './pages/Favorite'
-import {Toaster} from 'react-hot-toast'
+import React from'react';
+import Navbar from './components/Navbar';
+import {Routes,Route,useLocation} from 'react-router-dom';
 
+
+
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import SeatLayout from './pages/SeatLayout';
+import Favorite from './pages/Favorite';
+import MyBookings from './pages/MyBookings';
+import MovieDetails from './pages/MovieDetails';
+import {Toaster} from 'react-hot-toast';
 
 
 
 
 const App = ()=>{
-  const isAdminRoute=useLocation().pathname.startsWith('/admin')
-  return (
-    <>
-    <Toaster/>
-    {!isAdminRoute && <Navbar/>}
+const isAdminRoute=useLocation().pathname.startsWith('/admin'); 
+return (
+
+  
+  <>
+
+  <Toaster/>
+  {!isAdminRoute && <Navbar/>}
+
+  <Routes>
+  <Route path='/' element={<Home/>}  />
+  <Route path='/Movies' element={<Movies/>}  />
+  <Route path='/MoviesDetails/:id' element={<MovieDetails/>} />
+  <Route path='/MoviesDetails/:id/:date' element={<SeatLayout/>} />
+  <Route path='/MyBookings' element={<MyBookings/>} />
+  <Route path='/Favorite' element={<Favorite/>} />
+  </Routes>
+
+{!isAdminRoute && <Footer/>}
 
 
-<Routes>
-  <Route path = '/' element={<Home/>} />
-  <Route path = '/Movies' element={<Movies/>}  />
-  <Route path = '/Movies:id' element = {<MoviesDetails/>} />
-<Route   path = '/Movies:id:date' element= {<SeatLayout/>} />
-<Route   path = '/My-Bookings' element = {<MyBookings/>} />
-<Route   path = '/Favorite' element = {<Favorite/>} />
 
-
-</Routes>
-
-{!isAdminRoute && <footer/>}
     </>
-  )
+)
+
+
 }
-export default App
+export default App;
