6ef3d132dcc2a8f1321a5901c752847ec8380a50
Fahadkhan <fahadk8080@gmail.com>
2025-07-10 04:28:18 +0500

    vercel and inngest configuration has done

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { assets } from '../../assets/assets/assets'
 
+
 const AdminNavbar = () => {
+
+
   return (
     <div className='flex items-center px-6 md:px-10 h-16 border-b border-gray-300/30'>
     <Link to='/'>
