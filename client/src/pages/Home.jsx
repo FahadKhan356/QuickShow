ba0c44958154f571216b407b48e35b18170131ca
farhanali355 <fahadk8080@gmail.com>
2026-09-15 14:30:17 -0700

    fix(client): correct FeaturedSection import case (Vercel Linux is case-sensitive)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import HeroSection from '../components/HeroSection';
-import FeaturedSection from '../components/featuredSection';
+import FeaturedSection from '../components/FeaturedSection';
 import TrailerSection from '../components/TrailerSection';
 
 
